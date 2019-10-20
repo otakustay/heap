a0d5631f47099bf3324d05cad5b69c7d0c00bf7b
otakustay <otakustay@gmail.com>
2019-10-20 13:49:01 +0800

    use hsl based hue prioritized similar color algorithm

--- packages/similar-colors/lib/find-similar.js
+++ packages/similar-colors/lib/find-similar.js
@@ -5,17 +5,30 @@ const createPalette = require('./palette');
 
 const palette = createPalette();
 
-const mostSimilarColor = origin => minBy(palette, ({color}) => distance(origin, color.rgb().array()));
+// 单纯的用三维空间的距离找颜色会存在问题，就一个颜色来说，色调对人的感知要大于亮度和饱合度。
+// 因此这个算法使用HSL模型，并且在距离一定误差（1.1倍）范围内，以H轴距离最短为优先。
+const mostSimilarColor = origin => {
+    const minDistance = palette.reduce(
+        (min, {color}) => {
+            const current = distance(origin, color.hsl().array());
+            return Math.min(current, min);
+        },
+        Infinity
+    );
+    const allowedColors = palette.filter(({color}) => distance(origin, color.hsl().array()) <= minDistance * 1.1);
+    return minBy(allowedColors, ({color}) => Math.abs(color.hsl().object().h - origin[0]));
+};
 
 module.exports = input => {
     const color = new Color(input);
-    const rgb = color.rgb().array().slice(0, 3);
+    const hsl = color.hsl().array().slice(0, 3);
 
-    if (isEqual(rgb, [0, 0, 0]) || isEqual(rgb, [255, 255, 255])) {
+    // 纯白和纯黑不管
+    if (isEqual(hsl, [0, 0, 0]) || isEqual(hsl, [0, 0, 100])) {
         return null;
     }
 
-    const replacement = mostSimilarColor(rgb);
+    const replacement = mostSimilarColor(hsl);
     return {
         source: input,
         from: {color},
